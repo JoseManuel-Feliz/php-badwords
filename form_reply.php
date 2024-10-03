330cbb4298700d588f8e9b58f5f6522fec85ed36
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-03 09:12:59 +0200

    created & printed replace word variable

--- form_reply.php
+++ form_reply.php
@@ -1,22 +1,33 @@
 <?php
 $test = 'ok';
 $word = $_GET["word"];
+$replace_word = str_ireplace('precious', '***', $word);
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <title>php-badwords</title>
-    </head>
-    <body>
-<p>
-    <?php echo $word ?>
-</p>
-<p>
-    <?php echo 'il testo ha lunghezza ' . strlen($word) ?>
-</p>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+
+<body>
+
+    <div>
+        <p>
+            <?php echo $word ?>
+        </p>
+        <p>
+            <?php echo 'il testo ha lunghezza ' . strlen($word) ?>
+    </div>
+    <div>
+        <?php echo $replace_word ?>
+
+    </div>
+    </p>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -19,24 +19,27 @@ Lords of the rings, Gollum/Smeagol Monologue"
 
 ?>
 <! DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-badwords</title>
-</head>
-<body>
-<h1><?php echo $test ?></h1>
+    <html lang="en">
 
-<form action="./form_reply.php" method="GET">
-<div>
-<label for="word">Word:</label>
-<textarea id="word" type="text" name="word">
-<?php echo $defult_input ?>
-</textarea>
-</div>
-<button type="submit">Send</button>
-<button type="reset">Reset</button>
-</form>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php-badwords</title>
+    </head>
+
+    <body>
+        <h1><?php echo $test ?></h1>
+
+        <form action="./form_reply.php" method="GET">
+            <div>
+                <label for="word">Word:</label>
+                <textarea id="word" type="text" name="word">
+                    <?php echo $defult_input ?>
+                </textarea>
+            </div>
+            <button type="submit">Send</button>
+            <button type="reset">Reset</button>
+        </form>
+    </body>
+
+    </html>
