b7c16a4c15a3c3a0c5c11b747427e7ed91d3c09c
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-10-03 07:14:22 +0200

    added form

--- form_reply.php
+++ form_reply.php
@@ -1,3 +1,6 @@
+<?php
+$test = 'ok'
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,19 +9,6 @@
         <title>php-badwords</title>
     </head>
     <body>
-
+<h1><?php echo $test ?></h1>
     </body>
-</html>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
 </html>
