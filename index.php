49606c1799484c9d0f3e675cce287cca96888f32
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-10-03 07:14:22 +0200

    added form

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+$test = 'ok' 
+?>
 <! DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +9,15 @@
     <title>php-badwords</title>
 </head>
 <body>
+<h1><?php echo $test ?></h1>
 
+<form action="./form_reply" method="GET">
+    <div>
+<label for="word">Word:</label>
+        <input id="word" type="text" name="parola" placeholder="inserisci una parola">
+    </div>
+    <button type="submit">Send</button>
+    <button type="reset">Reset</button>
+    </form>
 </body>
 </html>
